be173ad41fcdd732b3f651dab02419d9d9333e3b
Kaushalji <harshitkaushal9129@gmail.com>
2026-02-25 13:28:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,38 +3,17 @@ const assets = [
   './',
   './index.html',
   './manifest.json',
-  './kaushalji.jpg',
-  'https://cdn.tailwindcss.com',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+  './kaushalji.jpg'
 ];
 
-// Install Service Worker
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Caching assets...');
-      return cache.addAll(assets);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
 });
 
-// Activate & Cleanup Old Caches
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== CACHE_NAME)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Fetching Assets
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cacheRes => {
-      return cacheRes || fetch(e.request);
-    })
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
