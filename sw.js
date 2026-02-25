017572055232356a5b15cf5a3de81fb3c6b2cb5c
Kaushalji <harshitkaushal9129@gmail.com>
2026-02-25 17:32:21 +0530

    Refactor service worker install and fetch event handlers

--- sw.js
+++ sw.js
@@ -1,7 +1,17 @@
+const CACHE_NAME = 'kaushal-calc-v1';
+const assets = ['./', './index.html', './manifest.json', './kaushalji.jpg'];
+
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(assets);
+        })
+    );
+    self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
+    );
 });
