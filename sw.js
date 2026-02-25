d3312d276d492ccd92c5f33c9019354c119881d7
Kaushalji <harshitkaushal9129@gmail.com>
2026-02-25 13:45:32 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'kaushal-pro-v2';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './kaushalji.jpg',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
