88b6876e51cf35159fb5307bfa41074145340f6c
Kaushalji <harshitkaushal9129@gmail.com>
2026-02-25 13:16:21 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'kaushal-pro-v2';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './kaushalji.jpg',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// Install Service Worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching assets...');
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Activate & Cleanup Old Caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetching Assets
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cacheRes => {
+      return cacheRes || fetch(e.request);
+    })
+  );
+});
