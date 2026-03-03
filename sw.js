6d3aadf929b6712b7592c6a36be2021d948f16fa
Kaushalji <harshitkaushal9129@gmail.com>
2026-03-03 11:29:18 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,15 @@
-const CACHE_NAME = 'mi-player-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://harshitkaushal9129-bit.github.io/Calculator/kaushalji.jpg'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('mi-player-v1').then((cache) => {
+      return cache.addAll(['/', 'index.html']); // Apni files ke naam yaha likhein
     })
   );
 });
 
-// Purane cache ko delete karne ke liye
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
     })
   );
 });
