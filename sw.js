89d43a28e696d25d882aedd2b30e4155472bf2a4
Kaushalji <harshitkaushal9129@gmail.com>
2026-03-03 17:52:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,24 @@
+const CACHE_NAME = 'mi-player-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://cdn-icons-png.flaticon.com/512/727/727218.png'
+];
+
+// Install Service Worker
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('mi-v1').then(c => c.addAll(['index.html'])));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
+
+// Fetch Assets
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
 });
