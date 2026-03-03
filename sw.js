831fba06eed450555a068b348c60d827ecfe693f
Kaushalji <harshitkaushal9129@gmail.com>
2026-03-03 11:38:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,6 @@
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('mi-player-v1').then((cache) => {
-      return cache.addAll(['/', 'index.html']); // Apni files ke naam yaha likhein
-    })
-  );
+  e.waitUntil(caches.open('mi-v1').then(c => c.addAll(['index.html'])));
 });
-
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
